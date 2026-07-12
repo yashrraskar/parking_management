8f5692fe2196d92bebc2819aec36df99239df1fa
yashrraskar <yashraskar2007@gmail.com>
2026-07-13 00:08:49 +0530

    Added parking feature

--- parking.cpp
+++ parking.cpp
@@ -3,7 +3,10 @@
 #include <cctype>
 using namespace std;
 
-const int parkingspots;
+const int parkingbike = 26;
+const int parkingcar = 14;
+int carCount = 0;
+int bikeCount = 0;
 
 class cardetails{
     string vehicletype,numberplate;
@@ -22,13 +25,36 @@ class cardetails{
         getline(cin, numberplate);
     }
 
+    void park(){
+        if (vehicletype == "car") {
+            if (carCount <= parkingcar){
+                cout << "THANKYOU !!"<<endl<<"Your parking slot number is - "<<carCount<<endl<<endl;
+                carCount++;
+            }else cout<<"SORRY NO PARKING SPOT AVAILABLE FOR CAR :("<<endl<<endl;
+        }
+        else if (vehicletype == "bike") {
+            if (bikeCount <= parkingbike){
+                cout << "THANKYOU !!"<<endl<<"Your parking slot number is - "<<bikeCount<<endl<<endl;
+                bikeCount++;
+            }else cout<<"SORRY NO PARKING SPOT AVAILABLE FOR BIKE :("<<endl<<endl;
+        }
+        else {
+        cout << "Invalid Vehicle Type!"<<endl<<endl;
+}
+    }
+
 
 };
 
 int main() {
 cout<<"\n========== PARKING MANAGEMENT SYSTEM ========== \n\n";
-class cardetails c1;
+class cardetails c1,c2,c3;
 c1.getinfo();
+c1.park();
+c2.getinfo();
+c2.park();
+c3.getinfo();
+c3.park();
 
     
 return 0;
